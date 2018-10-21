67180f8c7e48be738a3da381723243906b615e8b
E450 <yohoho233@gmail.com>
2018-10-21 22:24:44 +0800

    修改读取配置文件的错误

--- config.py
+++ config.py
@@ -5,18 +5,18 @@ import platform
 
 def config_get():
     if not os.path.exists('.config'):
-        default_config={}
+        default_config = {}
         system = platform.system()
-        if system=='Linux':
-            default_config['file_dir_path']= '/home/xhq/Papers/'
-            default_config['chrome_path']="evince"
-        elif system=='Windows':
-            default_config['file_dir_path']= 'C:/XLIB/Papers/'
-            default_config['chrome_path']="C:/Users/xhq/AppData/Local/Google/Chrome/Application/chrome.exe"
+        if system == 'Linux':
+            default_config['file_dir_path'] = '/home/xhq/Papers/'
+            default_config['chrome_path'] = "evince"
+        elif system == 'Windows':
+            default_config['file_dir_path'] = 'C:/XLIB/Papers/'
+            default_config['chrome_path'] = "C:/Users/xhq/AppData/Local/Google/Chrome/Application/chrome.exe"
         else:
             default_config['file_dir_path'] = './'
             default_config['chrome_path'] = "./"
-        json.dump(default_config, '.config')
+        json.dump(default_config, open('.config', 'w'))
         return default_config
-    current_config = json.load('.config')
+    current_config = json.load(open('.config', 'r'))
     return current_config
